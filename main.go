62bf8067ef7e1a5679b0e3499e8c2742d2696fcb
Dexter Williamson <dexter.williamson1@gmail.com>
2024-11-05 20:52:14 -0600

    working out the pathing with digitalocean vps

--- main.go
+++ main.go
@@ -28,16 +28,16 @@ func main() {
 	} else {
 		fmt.Println("Current working directory:", wd)
 	}
-	tmpl = template.Must(template.ParseGlob(wd + "/Website/templates/*.html"))
+	tmpl = template.Must(template.ParseGlob(wd + "/templates/*.html"))
 
 	h1 := func(w http.ResponseWriter, r *http.Request) {
 
 		blogs := map[string][]Blog{
 			"Blogs": {
-				{Title: "Go", Body: "Today feet good", Picture: wd + "/Website/assets/img/Go-Logo.png"},
-				{Title: "Java", Body: "Today feet good", Picture: wd + "/Website/assets/img/Java-Logo.png"},
-				{Title: "C++", Body: "Today feet good", Picture: wd + "/Website/assets/img/C++-Logo.png"},
-				{Title: "Python", Body: "Today feet good", Picture: wd + "/Website/assets/img/Python-Logo.png"},
+				{Title: "Go", Body: "Today feet good", Picture: "/assets/img/Go-Logo.png"},
+				{Title: "Java", Body: "Today feet good", Picture: "/assets/img/Java-Logo.png"},
+				{Title: "C++", Body: "Today feet good", Picture: "/assets/img/C++-Logo.png"},
+				{Title: "Python", Body: "Today feet good", Picture: "/assets/img/Python-Logo.png"},
 			},
 		}
 		tmpl.ExecuteTemplate(w, "index.html", blogs)
@@ -55,19 +55,19 @@ func main() {
 	}
 
 	http.HandleFunc("/templates/css/styles.css", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, wd+"/Website/templates/css/styles.css")
+		http.ServeFile(w, r, wd+"/templates/css/styles.css")
 	})
 	http.HandleFunc("/templates/js/scripts.js", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, wd+"/Website/templates/js/scripts.js")
+		http.ServeFile(w, r, wd+"/templates/js/scripts.js")
 	})
 	http.HandleFunc("/assets/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, wd+"/Website/assets"+r.URL.Path)
+		http.ServeFile(w, r, wd+r.URL.Path)
 	})
 
 	http.HandleFunc("/", h1)
 	http.HandleFunc("/add-blog/", h2)
 	http.HandleFunc("/spawnSVG/", func(w http.ResponseWriter, r *http.Request) {
-		filepaths := []string{wd + "/Website/assets/img/zig_zag.svg", wd + "/Website/assets/img/v.svg", wd + "/Website/assets/img/just_o.svg", wd + "/Website/assets/img/x.svg"}
+		filepaths := []string{wd + "/assets/img/zig_zag.svg", wd + "/assets/img/v.svg", wd + "/assets/img/just_o.svg", wd + "/assets/img/x.svg"}
 		randomPicker := rand.IntN(len(filepaths))
 		fmt.Println("served")
 		http.ServeFile(w, r, filepaths[randomPicker])
